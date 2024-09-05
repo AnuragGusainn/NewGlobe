b6fed8ef619f62d360f99e22303c31d87f903d88
AnuragGusainn <86362000+AnuragGusainn@users.noreply.github.com>
2024-09-05 19:57:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/lesson-complete', methods=['GET'])
 def lesson_complete():
-    return jsonify({"status": "Lesson Completed 70%"}), 200
+    return jsonify({"status": "Lesson Completed 90%"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
