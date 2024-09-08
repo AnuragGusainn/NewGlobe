463b6705d904c372d75e8f7ebfd4783c1f83b18a
AnuragGusainn <86362000+AnuragGusainn@users.noreply.github.com>
2024-09-08 23:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask, jsonify, render_template_string
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Route to display lesson completion status with image
 @app.route('/lesson-complete', methods=['GET'])
 def lesson_complete():
-    # Create HTML content with an image and a comment
-    html_content = '''
-    <html>
-        <body>
-            <h1>Lesson Completed: 70%</h1>
-            <img src="{{ url_for('NewGlobe', filename='demo_image.jpeg') }}" alt="Demo Image" width="300">
-        </body>
-    </html>
-    '''
-    return render_template_string(html_content)
+    return jsonify({"status": "Lesson Completed 75%"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
