801ec0aa33fd1e04e49d866e5c5563b1a67afba9
Anurag Gusain <gusainanurag9@gmail.com>
2024-09-23 11:47:48 +0530

    app.py v4

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def lesson_complete():
             </style>
         </head>
         <body>
-            <div class="status">Lesson Completed: 95%</div>
+            <div class="status">Lesson Completed: 99%</div>
         </body>
     </html>
     '''
